62fe6f3d12b9166c21fed09af01a1464bbdff57e
Carlos Ambeliz <carlosambeliz@hotmail.com>
2021-09-29 21:10:28 -0600

    Allow cors

--- Startup.cs
+++ Startup.cs
@@ -21,6 +21,11 @@ namespace dw.UserService
         {
             services.AddDiscoveryClient(Configuration);
             services.AddControllers();
+            services.AddCors(options=>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                     builder => builder.AllowAnyOrigin());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +38,7 @@ namespace dw.UserService
 
             app.UseHttpsRedirection();
             app.UseRouting();
+            app.UseCors("AllowAllOrigins");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
