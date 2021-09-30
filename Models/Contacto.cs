a4db407d4bb33d6ef82df81046223a4e55eb1ce1
Carlos Ambeliz <carlosambeliz@hotmail.com>
2021-09-29 20:17:38 -0600

    Se agrego contexto EF Database fist aproach

---
+++ Models/Contacto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace dw.UserService.Models
+{
+    public partial class Contacto
+    {
+        public long IdContacto { get; set; }
+        public long IdUsuario { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace dw.UserService.Models
+{
+    public partial class Usuario
+    {
+        public long IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
